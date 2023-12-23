707f4100ff9596bb74a68255c5ee83e5c9ecbb92
CoffeeNStuff <CoffeeNStuff0@gmail.com>
2023-12-22 23:59:18 -0500

    initial commit

---
+++ lua/edit/edit.lua
@@ -0,0 +1,14 @@
+function edit () 
+	local settings = getCurrSet() 
+
+	while isOpen do
+		if settings.autoWidth then 
+
+		end 
+	end 
+end 
+	
+local function null () 
+end 
+
+vim.api.nvim_open_win(0, true, null())
