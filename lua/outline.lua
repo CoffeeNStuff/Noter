8627fcf4e81caa9a81acd2dd24fcd5d05b51f3a2
CoffeeNStuff <CoffeeNStuff0@gmail.com>
2024-01-03 21:21:41 -0500

    Jan 3

--- lua/outline.lua
+++ lua/outline.lua
@@ -37,6 +37,10 @@ O.init = function ()
 		O.Header2 = sets.Header2
 		O.Point = sets.Point
 		O.Question = sets.Questions
+
+		-- get array of markers from settings (stored as simple array of { t, pir })
+		-- sort by length of  t
+		-- have function that compares in proper order 
 	]]
 
 		O.TabLength = 3
@@ -114,6 +118,16 @@ O.refreshOutline = function()
 			O.currLines[#O.currLines + 1] = string.rep('\t', O.sections[i][2] - O.currDepth) .. O.sections[i][4]
 		end 
 	end
+
+	if O.currDepth == 0 then 
+		if #vim.fn.expand('%') > 40 then 
+			table.insert(O.currLines, 1, string.sub(vim.fn.expand('%'), 1, 35) .. ' ... ') 
+			table.insert(O.currLines, 2, string.rep('-', 40)) 
+		else 
+			table.insert(O.currLines, 1, vim.fn.expand('%')) 
+			table.insert(O.currLines, 2, string.rep('-', #vim.fn.expand('%'))) 
+		end
+	end 
 end 
 
 function noterClose () 
@@ -124,7 +138,25 @@ function noterClose ()
 	end 
 end 
 
+-- Wowzahs FIXME 
 function noterSnap () -- snap to current location
+	currLine = ui.nvim_win_get_cursor(O.NotesWindow)[1]
+
+	for i = 1, #O.sections do
+		if O.sections[i][3] > currLine then
+			local point = O.sections[i]
+			if point[2] == 1 then 
+				-- TODO 
+			else 
+				while i > 1 do 
+					i = i - 1
+					if O.sections[i][2] == point[2] - 1 then
+
+					end 
+				end 
+			end 
+		end 
+	end 
 end 
 
 function  noterRegress () 
@@ -151,14 +183,55 @@ function  noterRegress ()
 	O.currSections = newSections
 	O.currLines = newLines
 
+	--[[ if O.currDepth == 0 then 
+		if #vim.fn.expand('%') > 40 then 
+			table.insert(O.currLines, 1, string.sub(vim.fn.expand('%'), 1, 35) .. ' ... ') 
+			table.insert(O.currLines, 2, string.rep('-', 40)) 
+		else 
+			table.insert(O.currLines, 1, vim.fn.expand('%')) 
+			table.insert(O.currLines, 2, string.rep('-', #vim.fn.expand('%'))) 
+		end
+	else 
+		local i = newSections[1][5]
+		local header = nil 
+
+		while (i > 1) do 
+			i = i - 1
+			if O.sections[i][2] == O.currDepth - 1 then 
+				header = O.sections[i]
+				break
+			end 
+		end 
+		if #header[4] > 40 then
+			table.insert(O.currLines, 1, string.sub(header[4], 1, 35) .. ' ... ')
+			table.insert(O.currLines, 2, string.rep('-', 40)) 
+		else 
+			table.insert(O.currLines, 1, header[4])
+			table.insert(O.currLines, 2, string.rep('-', #newLines[1]))
+		end 
+	end
+	]] 
+
 	ui.nvim_buf_set_lines(O.buf, 0, #O.currLines, false, {}) 
 	ui.nvim_buf_set_lines(O.buf, 0, 5, false, O.currLines) 
 end 
 
 function  noterProgress () 
 
-	local currPoint = O.currSections[ui.nvim_win_get_cursor(O.win)[1]] 
-	local absPoint = O.sections[currPoint[5]]
+	local currLine = ui.nvim_win_get_cursor(O.win)[1] - 2-- in the case cursor hovers non-section
+	if currLine < 0 then 
+		print 'no subsections' 
+		return
+	end 
+
+	local absPoint = O.sections[O.currSections[currLine - 2][5]] -- in case there is no further sections
+	if absPoint[5] + 1 > #O.sections then
+		print 'no subsections' 
+		return
+	elseif O.sections[absPoint[5] + 1][2] ~= absPoint[2] + 1 then -- in case there are no subsections
+		print 'no subsections' 
+		return
+	end 
 
 	local newSections = {} 
 	for i = absPoint[5] + 1, #O.sections do 
@@ -174,18 +247,14 @@ function  noterProgress ()
 		newLines[#newLines + 1] = string.rep('\t', newSections[i][2] - (absPoint[2] + 1)) .. newSections[i][4]
 	end 
 
-	if #currPoint[4] > 40 then
-		table.insert(newLines, 1, string.sub(currPoint[4], 1, 35) .. ' ... ')
+	if #absPoint[4] > 40 then
+		table.insert(newLines, 1, string.sub(absPoint[4], 1, 35) .. ' ... ')
 		table.insert(newLines, 2, string.rep('-', 40)) 
 	else 
-		table.insert(newLines, 1, currPoint[4])
+		table.insert(newLines, 1, absPoint[4])
 		table.insert(newLines, 2, string.rep('-', #newLines[1]))
 	end 
 
-	if #newSections == 0 then
-		print 'no subsections' 
-		return
-	end 
 
 	O.currDepth = O.currDepth + 1
 	O.currSections = newSections
@@ -193,38 +262,36 @@ function  noterProgress ()
 
 	ui.nvim_buf_set_lines(O.buf, 0, #O.currLines, false, {}) 
 	ui.nvim_buf_set_lines(O.buf, 0, #O.currLines, false, O.currLines) 
-
 end 
 
 -- lul 
 	-- lul 2
 		-- lul 3
 function  noterToggle () 
-	local currLine = ui.nvim_win_get_cursor(O.win)[1]
-	local currPoint = O.currSections[currLine] 
-	local absPoint = O.sections[currPoint[5]]
+	local currLine = ui.nvim_win_get_cursor(O.win)[1] - 2
+	local absPoint = O.sections[O.currSections[currLine][5]]
 
-	print (O.sections[absPoint[5] + 1][4])
-
-	if currLine == #O.currSections or O.currSections[currLine + 1][2] == currPoint[2] + 1 then -- close point 
+	if currLine == #O.currSections then  -- in case there are no further subsections
+			print 'no subsections' 
+			return  
+	elseif O.currSections[currLine + 1][2] == absPoint[2] + 1 then -- close point 
 		for i = currLine + 1, #O.currSections do
-			if O.currSections[i][2] < currPoint[2] then
+			if O.currSections[i][2] < absPoint[2] then
 				break
 			end 
 		end 
 
 		table.remove(O.currSections, currLine + 1, i) 
-		table.remove(O.currLines, currLine + 1, i) 
+		table.remove(O.currLines, currLine + 3, i) 
 
 		ui.nvim_buf_set_lines(O.buf, 1, #O.currLines, false, O.currLines) 
-
 	elseif O.sections[absPoint[5] + 1][2] == absPoint[2] + 1 then -- open point
 
 		local newSections = {} 
 		for i = absPoint[5] + 1, #O.sections do 
-			if O.sections[i][2] == O.currDepth + 1 then
+			if O.sections[i][2] == absPoint[2] + 1 then
 				newSections[#newSections + 1] = O.sections[i]
-			elseif O.sections[i][2] < currPoint[2] then
+			elseif O.sections[i][2] < absPoint[2] then
 				break
 			end 
 		end
@@ -236,7 +303,7 @@ function  noterToggle ()
 		end 
 
 		table.insert(O.currSections, currLine, newSections) 
-		table.insert(O.currLines, currLine, newLines) 
+		table.insert(O.currLines, currLine + 3, newLines) 
 
 		ui.nvim_buf_set_lines(O.buf, 0, #O.currLines, false, O.currLines) 
 	else 
@@ -247,7 +314,7 @@ end
 
 function noterSubmit () 
 	local i = ui.nvim_win_get_cursor(O.win)[1]
-	-- noterClose() 
+	noterClose() 
 	ui.nvim_win_set_cursor(O.NotesWindow, { O.currSections[i][3], O.currSections[i][2] + 1})	
 end 
 
